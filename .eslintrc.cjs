c383c9a829bef746189233ccd48421f3d6f209e5
LemonNeko <chheese048@gmail.com>
2022-04-21 22:32:08 +0800

    style: 更换 eslint 配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,7 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-    extends: [
-      'plugin:vue/vue3-strongly-recommended',
-      '@vue/eslint-config-typescript/recommended'
-    ],
-    ignorePatterns: [
-      'node_modules',
-      'dist',
-      'src/assets'
-    ],
-    rules: {
-      '@typescript-eslint/semi': ['error', 'never']
-    }
-  }
+  extends: [
+    '@antfu',
+  ],
+}
